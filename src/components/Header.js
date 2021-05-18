4238f37f8692cc9738cbe6f8c6464cc6f1edf90f
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-05-18 15:39:09 -0400

    search input and logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
-import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
     return (
         <div className="header">
+
+<       img
+		    className="youtube-logo"
+		    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png"
+		    alt="YouTube logo"
+		/>
+
+        <div className="header-search">
+            <input type="text"/>
+            <SearchIcon />
+        </div>
 
         </div>
     )
