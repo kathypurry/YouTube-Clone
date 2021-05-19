741a14f2acd21a1a370b88e6a32b4ce20d8c582f
Kathy Gee <mzchick@gmail.com>
2021-05-19 15:47:48 -0400

    addtional About sections

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h1></h1>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="desc">
+        <h1>Project Description</h1>
+        <p>
+          Using React.JS, the Youtube API, and other libraries, We created our
+          version of a Youtube Clone! We decided to incorporate class
+          components, React Hooks, and React Styling to solidify the language
+          that we are currently practicing in.
+        </p>
+      </div>
+
+      <div>
+        <h1>Candy</h1>
+        <p>I'm leaning on a fence between past and present tense</p>
+
+        <h1>Kathy</h1>
+        <p>I love the color pink and Popeyes Chicken</p>
+      </div>
+    </>
+  );
+};
 
 export default About;
