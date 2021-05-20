d602cc97c96928138804644433dfa70112a0ae90
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-05-20 13:17:36 -0400

    .env file, request func export

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-const YTKEY = 'AIzaSyCe-AABqG4PxfGpdJ8RDjV4XS0NuXg398I';
-
-export default axios.create({
-    baseUrl: 'https://www.googleapis.com/youtube/v3/',
+const request = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
     params: {
-        part: 'snippet',
-        maxResults: 10,
-        key: YTKEY
-
-    }
+        key: YouTube_API_KEY,
+    },
 })
+
+export default request;
