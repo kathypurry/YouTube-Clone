0434c806e31b617c770ba320cbfe536872c1d33e
Kathy Gee <mzchick@gmail.com>
2021-05-24 13:00:03 -0400

    moved videodata to play

--- src/App.js
+++ src/App.js
@@ -1,72 +1,79 @@
 import Header from "./components/Header";
-import {Route, Switch, Link} from 'react-router-dom';
-import './App.css';
-import About from './components/About';
-import React, { Component } from 'react';
+import { Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import React, { Component } from "react";
 import Search from "./components/Search";
 import youtube from "./api/youtube";
 import VideoData from "./components/VideoData";
 import VideoPlayer from "./components/VideoPlayer";
-import axios from 'axios';
-import Play from './components/Play'
-
+import axios from "axios";
+import Play from "./components/Play";
 
 document.onkeypress = function (e) {
   e = e || window.event;
   if (e.keyCode === 12) {
     document.documentElement.classList.toggle("dark-mode");
-    document.querySelectorAll('.inverted').forEach((result) => {
-      result.classList.toggle('invert');
-    })
+    document.querySelectorAll(".inverted").forEach((result) => {
+      result.classList.toggle("invert");
+    });
   }
-}
-console.log(process.env.REACT_APP_API_KEY)
+};
+console.log(process.env.REACT_APP_API_KEY);
 class App extends Component {
   state = {
     videoInfo: [],
     selectedVideoId: null,
-  }
-  onVideoSelected = videoId => {
-    this.setState({ selectedVideoId: videoId})
-  }
-  onSearch = async keyword => {
+  };
+  onVideoSelected = (videoId) => {
+    this.setState({ selectedVideoId: videoId });
+  };
+  onSearch = async (keyword) => {
     const response = await youtube.get("./search", {
       params: {
         q: keyword,
-      }
-    })
+      },
+    });
     this.setState({
       videoInfo: response.data.items,
-      selectedVideoId: response.data.items[0].id.videoId
-    })
+      selectedVideoId: response.data.items[0].id.videoId,
+    });
     // console.log(this.state)
-  }
+  };
   render() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path = "/"/>
-        <Route exact path= "/video" component={Play}></Route>
-        <Route exact path= "/About" component={About} />
-      </Switch>
-      <Search onSearch={this.onSearch} />
-      <Link to="/video" target="_blank" onClick={(event) => {
-      event.preventDefault();
-      window.open(this.makeHref("/video"));
-      }} >
-      <VideoData target="_blank" className="inverted" onVideoSelected={this.onVideoSelected} data={this.state.videoInfo} /> </Link>
-      <VideoPlayer target="_blank" videoId={this.state.selectedVideoId} />
-    </div>
-  );
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" />
+          <Route
+            exact
+            path="/video"
+            render={() => (
+              <Play
+                onVideoSelected={this.onVideoSelected}
+                data={this.state.videoInfo}
+                videoId={this.state.selectedVideoId}
+              ></Play>
+            )}
+          ></Route>
+          <Route exact path="/About" component={About} />
+        </Switch>
+        <Search onSearch={this.onSearch} />
+      </div>
+    );
   }
 }
 export default App;
 
-<Link to="route" target="_blank" onClick={(event) => {
-  event.preventDefault();
-  window.open(this.makeHref("route"));
-}} />
+<Link
+  to="route"
+  target="_blank"
+  onClick={(event) => {
+    event.preventDefault();
+    window.open(this.makeHref("route"));
+  }}
+/>;
 
 // import Header from "./components/Header";
 // import {Route, Switch} from 'react-router-dom';
@@ -134,6 +141,5 @@ export default App;
 //     </div>
 //   );
 //   }
-
 
 // export default App;
--- src/components/Play.js
+++ src/components/Play.js
@@ -1,6 +1,37 @@
 import React, {Component} from 'react'
 import VideoPlayer from './VideoPlayer.js'
+import VideoData from './VideoData.js'
 
-class Play extends Component {}
+
+class Play extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            onVideoSelected: props.onVideoSelected,
+            data: props.data,
+            videoId: props.selectedVideoId,
+        }
+    } 
+    
+    render() 
+    { console.log(this.state)
+        console.log("state is")
+    return (
+
+
+    <div>
+        <h1>moo</h1>
+         <VideoData
+            target="_blank"
+            className="inverted"
+            onVideoSelected={this.state.onVideoSelected}
+            data={this.state.data}
+          />{" "}
+      
+        <VideoPlayer target="_blank" videoId={this.state.videoId} />
+    </div>
+)
+}
+}
 
 export default Play
