754776f64df4f3f2b6247b44cd989713587fa863
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-05-25 19:02:42 -0400

    Changed app theme, centered elements on page, logo added, video player style changed

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -5,7 +5,6 @@ import YouTube from "react-youtube";
 class Videoplayer extends Component {
 
   videoOnReady(event) {
-    // access to player in all event handlers via event.target
     event.target.pauseVideo();
   }
 
@@ -14,21 +13,19 @@ class Videoplayer extends Component {
       height: '390',
       width: '640',
       playerVars: {
-        // https://developers.google.com/youtube/player_parameters
         autoplay: 1,
       },
     };
 
 
     const {videoId} = this.props
-
-    if (!videoId) {
-      return (
-        <p className="v-player-text">
-          Search for video
-        </p>
-      )
-    }
+      if (!videoId) {
+        return (
+          <p className="v-player-text">
+            Search for video
+          </p>
+        )
+      }
 
     return <YouTube videoId={videoId} opts={opts} videoOnReady={this.videoOnReady} />;
 
