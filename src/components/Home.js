40c058919a758d480e8e98b5f28ca6f122b97157
Kathy Gee <mzchick@gmail.com>
2021-05-26 09:57:54 -0400

    video/:id

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,41 +2,50 @@ import React, { Component } from "react";
 import Search from "./Search";
 import youtube from "./youtube";
 import VideoData from "./VideoData";
-// import VideoPlayer from "./VideoPlayer";
+import CK from "./CK.gif";
+import VideoPlayer from "./VideoPlayer";
 
 export default class Home extends Component {
-    state = {
-        videoInfo: [],
-        selectedVideoId: null,
-      }
+  state = {
+    videoInfo: [],
+    selectedVideoId: null,
+  };
 
-      onVideoSelected = videoId => {
-        this.setState({ selectedVideoId: videoId})
-      }
+  onVideoSelected = (videoId) => {
+    this.setState({ selectedVideoId: videoId });
+  };
 
-      onSearch = async keyword => {
-        const response = await youtube.get("./search", {
-          params: {
-            q: keyword,
-          }
-        })
-        this.setState({
-          videoInfo: response.data.items,
-          selectedVideoId: response.data.items[0].id.videoId
-        })
-      }
-      render() {
-      return (
-          <div>
-              <Search onSearch={this.onSearch} />
-              <div className="v-player">
-             <p className="v-player-text">
-              Search for video
-            </p>
-              <VideoData base target="_blank" className="inverted" onVideoSelected={this.onVideoSelected} data={this.state.videoInfo} />
-              {/* <VideoPlayer base target="/video/v" videoId={this.state.selectedVideoId} /> */}
+  onSearch = async (keyword) => {
+    const response = await youtube.get("./search", {
+      params: {
+        q: keyword,
+      },
+    });
+    this.setState({
+      videoInfo: response.data.items,
+      selectedVideoId: response.data.items[0].id.videoId,
+    });
+    // console.log(this.state)
+  };
+  render() {
+    return (
+      <div>
+        <Search onSearch={this.onSearch} />
+        <div className="v-player">
+          <p className="v-player-text">Search for video</p>
+          <h1>Welcome to CandyKat</h1>
+
+          <VideoData
+            base
+            target="_blank"
+            className="inverted"
+            onVideoSelected={this.onVideoSelected}
+            data={this.state.videoInfo}
+          />
+                    <img src={CK} />
+          {/* <VideoPlayer base target="_blank" videoId={this.state.selectedVideoId} /> */}
         </div>
-        </div>
-      );
-    }
+      </div>
+    );
+  }
 }
